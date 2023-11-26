002dcebe6320edc8dd1c75812cff34d66c575cf2
Ayush Singh <99062720+ayush-singh05@users.noreply.github.com>
2023-11-26 16:34:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+let student = {
+	name: "ayush",
+}
+
+
+
+Object.prototype.getKeys = function() {
+  return Object.keys(this);
+};
+
+
+const keys = student.getKeys();
+
+console.log(keys); 
